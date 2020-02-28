ba8d4d24210b218922ce409145802b79ecb04804
English-Ghost <56802968+English-Ghost@users.noreply.github.com>
2020-02-27 18:19:46 -0600

    2/27/2020
    added a frame to the GUI

--- javaGuiCode/baseCode.java
+++ javaGuiCode/baseCode.java
@@ -1,8 +1,19 @@
-class baseCode
-{
 
-  public static void main(String args[])
-  {
-    System.out.println("Running the GUI");
+import java.sql.*;
+import javax.swing.*;
+public class baseCode {
+  public static void main(String[] args) {
+
+    JFrame f=new JFrame("Sports Database");//creating instance of JFrame
+
+    JButton b=new JButton("Connect to server");//creating instance of JButton
+    b.setBounds(130,100,100, 40);//x axis, y axis, width, height
+
+    f.add(b);//adding button in JFrame
+
+    f.setSize(600,300);//400 width and 500 height
+    f.setLayout(null);//using no layout managers
+    f.setVisible(true);//making the frame visible
+
   }
 }
